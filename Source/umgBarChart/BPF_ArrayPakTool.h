8e6c5c357e5aaad854c35d96249251f8fcbe5031
halfgear <1964625940@qq.com>
2024-05-19 11:26:29 +0800

    first commit

---
+++ Source/umgBarChart/BPF_ArrayPakTool.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BPF_ArrayPakTool.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UMGBARCHART_API UBPF_ArrayPakTool : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	
+};
